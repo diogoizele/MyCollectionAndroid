835cb0db4da2a3e128bf0a56ab56ede693448eb9
diogoizele <diogogabrielizele@gmail.com>
2025-11-06 21:05:48 -0300

    refactor: rename dashboard module to collections
    Renames the `dashboard` module to `collections`.
    
    This includes renaming the following files and their corresponding classes:
    - `DashboardFragment` -> `CollectionsFragment`
    - `DashboardViewModel` -> `CollectionsViewModel`
    - `fragment_dashboard.xml` -> `fragment_collections.xml`

--- app/src/main/java/com/diogo/mycollection/ui/collections/CollectionsFragment.kt
+++ app/src/main/java/com/diogo/mycollection/ui/collections/CollectionsFragment.kt
@@ -1,4 +1,4 @@
-package com.diogo.mycollection.ui.dashboard
+package com.diogo.mycollection.ui.collections
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.diogo.mycollection.databinding.FragmentDashboardBinding
+import com.diogo.mycollection.databinding.FragmentCollectionsBinding
 
-class DashboardFragment : Fragment() {
+class CollectionsFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentCollectionsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val collectionsViewModel =
+            ViewModelProvider(this).get(CollectionsViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentCollectionsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        collectionsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
