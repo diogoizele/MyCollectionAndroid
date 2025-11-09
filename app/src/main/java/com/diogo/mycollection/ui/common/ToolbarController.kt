f06db89233d4a3913662476f66a5020c9f47eb17
diogoizele <diogogabrielizele@gmail.com>
2025-11-09 17:44:17 -0300

    feat: add home toolbar and update nav icons
    Adds a new toolbar menu for the home screen with search and profile actions. Introduces a `ToolbarController` to manage the toolbar's state, including its title, menu, and navigation icon, based on the current navigation destination.
    
    Also updates the bottom navigation icons for "home" and "collections" and adjusts the main layout and manifest for better toolbar and keyboard behavior.

---
+++ app/src/main/java/com/diogo/mycollection/ui/common/ToolbarController.kt
@@ -0,0 +1,65 @@
+package com.diogo.mycollection.ui.common
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import com.diogo.mycollection.R
+
+class ToolbarController(
+    private val activity: AppCompatActivity,
+    private val toolbar: Toolbar,
+    private val navController: NavController
+) {
+    fun setup () {
+        activity.setSupportActionBar(toolbar)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.navigation_home -> {
+                    update(
+                        title = "My Collection",
+                        menu = R.menu.home_menu,
+                        back = false,
+                        navigationIcon = R.drawable.ic_layer_group_solid_full
+                    )
+                }
+                else -> {
+                    update(
+                        title = destination.label?.toString(),
+                        menu = null,
+                        back = true
+                    )
+                }
+            }
+        }
+    }
+
+    private fun update(
+        title: String?,
+        menu: Int?,
+        back: Boolean,
+        navigationIcon: Int? = null
+    ) {
+        activity.supportActionBar?.apply {
+            title?.let { this.title = it }
+            setDisplayHomeAsUpEnabled(back)
+        }
+
+        toolbar.navigationIcon = when {
+            back -> null
+            navigationIcon != null -> AppCompatResources.getDrawable(activity, navigationIcon)
+            else -> null
+        }
+
+        if (activity is ToolbarMenuHandler) {
+            activity.currentMenuRes = menu
+        }
+
+        activity.invalidateOptionsMenu()
+    }
+}
+
+interface ToolbarMenuHandler {
+    var currentMenuRes: Int?
+}
